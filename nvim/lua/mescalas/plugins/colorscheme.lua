0943ef93625bc15365253efdae63214a2d95e35c
Matéo Escalas <mateo.escalas@oppus.fr>
2024-09-22 22:38:37 +0200

    setup colotscheme in nvim

--- nvim/lua/mescalas/plugins/colorscheme.lua
+++ nvim/lua/mescalas/plugins/colorscheme.lua
@@ -15,6 +15,7 @@ return {
 
       require("tokyonight").setup({
         style = "night",
+        transparent = true,
         on_colors = function(colors)
           colors.bg = bg
           colors.bg_dark = bg_dark
