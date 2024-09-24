7430f417e7e0630857c5b5926af399879d8c3eb8
Matéo Escalas <mateo.escalas@oppus.fr>
2024-09-24 18:42:44 +0200

    added a bunch of plugins for nvim configuration

--- nvim/lua/mescalas/lazy.lua
+++ nvim/lua/mescalas/lazy.lua
@@ -11,4 +11,12 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("mescalas.plugins")
+require("lazy").setup({ { import = "mescalas.plugins"}, { import = "mescalas.plugins.lsp" } }, {
+    checker = {
+      enabled = true,
+      notify = false,
+    },
+    change_detection = { 
+      notify = false,
+    },
+});
